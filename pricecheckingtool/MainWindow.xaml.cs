9f086f3d3921dcb6d322bc17c78c572522ec44a9
DESKTOP-8TUMM4T\Philipp <philipp.thamm@stud.hshl.de>
2019-07-04 00:23:11 +0200

    user model created

--- pricecheckingtool/MainWindow.xaml.cs
+++ pricecheckingtool/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace pricecheckingtool
 {
@@ -23,6 +24,12 @@ namespace pricecheckingtool
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User(sessionID.Text, accName.Text);
+            MessageBox.Show($"Willkommen {user.accName}");
         }
     }
 }
---
+++ pricecheckingtool/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pricecheckingtool
+{
+    class User
+    {
+        public string sessionID { get; set; }
+        public string accName { get; set; }
+
+        public User(string sessionID, string accName)
+        {
+            this.sessionID = sessionID;
+            this.accName = accName;
+
+            WriteToFile();
+        }
+
+        private void WriteToFile()
+        {
+        
+            string path = @"C:\Users\Philipp\Documents\pricecheckingtool\pricecheckingtool\user.txt";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    tw.WriteLine($"sessionID: {sessionID}, accName: {accName}");
+                    tw.Close();
+                }
+            }
+        }
+    }
+}
