0f65d5f22d0b709bb03e649a7840921669a2ccec
skor1994 <philipp.thamm@stud.hshl.de>
2019-10-14 23:56:30 +0200

    added web api project

---
+++ api/pricecheckingtoolapi/pricecheckingtoolapi/Controllers/PartiesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pricecheckingtoolapi.Db;
+using pricecheckingtoolapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pricecheckingtoolapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PartiesController : ControllerBase
+    {
+        private readonly DatabaseContext databaseContext;
+        public PartiesController(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Party>> GetSingleParty(int id)
+        {
+            var party = await databaseContext.Partys.FindAsync(id);
+
+            if (party == null)
+            {
+                return NotFound();
+            }
+
+            return party;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<IEnumerable<Party>>> GetAll()
+        {
+            return await databaseContext.Partys.ToListAsync(); ;
+        }
+    }
+}
---
+++ api/pricecheckingtoolapi/pricecheckingtoolapi/Models/PartyUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pricecheckingtoolapi.Models
+{
+    public class PartyUser
+    {
+        public int partyId { get; set; }
+        public Party party { get; set; }
+
+        public int userId { get; set; }
+        public User user { get; set; }
+    }
+}
